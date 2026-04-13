f0d55fb618cfbd36015b733fd6cda0d7e4086ecf
H.Minh <nhatminhdt23@gmail.com>
2026-04-13 20:23:45 +0700

    Init

---
+++ hedi/src/test/java/com/group/thr/hedi/HediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.thr.hedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
