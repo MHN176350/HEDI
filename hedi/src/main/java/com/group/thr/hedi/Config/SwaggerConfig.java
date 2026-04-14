3458aca01a55e7a9bbdbceb34c669ecf09b2f9a0
Meer <nhatminhdt23@gmail.com>
2026-04-14 15:41:04 +0700

    Jwt+Base Package

---
+++ hedi/src/main/java/com/group/thr/hedi/Config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.group.thr.hedi.Config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Student Permission Management System API")
+                        .version("1.0.0")
+                        .description("Health Diary APIs")
+                        .contact(new Contact()
+                                .name("Development Team")
+                                .email("dev@example.com")
+                                .url("https://example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
