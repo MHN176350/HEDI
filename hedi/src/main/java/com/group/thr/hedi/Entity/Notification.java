b60e23bb4250990cbf76dcf6481c13ab4a89d869
kietTD <trandanhkiet160903@gmail.com>
2026-05-06 14:07:41 +0700

    updated

--- hedi/src/main/java/com/group/thr/hedi/Entity/Notification.java
+++ hedi/src/main/java/com/group/thr/hedi/Entity/Notification.java
@@ -7,6 +7,8 @@ import com.group.thr.hedi.Enum.NotificationType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,23 +18,25 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name = "notifications")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Notification {
-@Id
-@GeneratedValue( strategy =  GenerationType.IDENTITY)
-private Long id;
-@Column(nullable = false)
-private NotificationType type;
-@Column(nullable = false)
-private String message;
-@Column(nullable = false)
-private boolean isRead=false;
-@Column(name = "created_at", updatable = false)
-private LocalDateTime createdAt;
-@ManyToOne(fetch=FetchType.LAZY)
-private User user;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NotificationType type;
+    @Column(nullable = false)
+    private String message;
+    @Column(nullable = false)
+    private boolean isRead = false;
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
